3726a97a431d2434217e461418dd82871a5db4f6
Sem Kholodov <kholodovsem@gmail.com>
2022-08-02 05:14:13 +0300

    End

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,29 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { Component } from 'react';
+import SearchBar from './SearchBar/Searchbar';
+import ImageGallery from './ImageGallery/ImageGallery';
+import Modal from './modal/Modal';
+
+export class App extends Component {
+  state = {
+    require: '',
+  };
+
+  onSubmit = (require) => {
+    this.setState({require});
+  }
+
+  render() {
+    const { require } = this.state;
+
+    return (
+      <main>
+        <SearchBar onSubmit={this.onSubmit}/>
+        <ImageGallery require={require} />
+        <ToastContainer />
+      </main>
+    );
+  }
 };
+
